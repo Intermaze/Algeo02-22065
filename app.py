8cc4c5effdab1016255a8ac5f174349b75b4984b
Hanief Nashrullah <hanieffatkhan@gmail.com>
2023-11-11 18:45:01 +0700

    Added Vector Loading for HSV

--- app.py
+++ app.py
@@ -2,11 +2,19 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import subprocess
 import os
+import numpy as np
+import load_features
  
 app = Flask(__name__)
  
 upload_folder = os.path.join('static', 'uploads')
- 
+
+# Loading pre-processed features
+global hsv_features
+hsv_features = load_features.load_array()
+print(hsv_features)
+
+
 app.config['UPLOAD'] = upload_folder
  
 @app.route('/', methods=['GET', 'POST'])
@@ -19,10 +27,16 @@ def upload_file():
         return render_template('index.html', img=img)
     return render_template('index.html')
 
+
 @app.route('/extract_features', methods=['POST'])
 def extract_features():
+    global hsv_features
+    load_features.del_hsv()
     result = subprocess.check_output(['python', 'feature_extraction.py'], stderr=subprocess.STDOUT)
     result = result.decode('utf-8')
+    hsv_features = load_features.load_array()
+    print(hsv_features)
+    print("NORMAL")
     return render_template('index.html',extracted_notice=result)
  
 if __name__ == '__main__':
---
+++ load_features.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+
+def load_array():
+    features_dir = 'dataset_features/hsv/'
+    array_of_hsvfeatures = []
+    for features in os.listdir(features_dir):
+        namafileHSV = features_dir+features
+        feat = np.load(namafileHSV)
+        array_of_hsvfeatures.append([feat,features[:-4]])
+    return array_of_hsvfeatures
+
+def del_hsv():
+    for item in os.listdir('dataset_features/hsv/'):
+        os.remove('dataset_features/hsv/'+item)
